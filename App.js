205e4d5ff2668fcabe79e1b1657436cdde53bd95
Baby mumthas <152773115+BabyMumthas@users.noreply.github.com>
2025-02-03 21:59:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterValue, setFilterValue] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the server
+    axios
+      .get('http://localhost:3001/persons') // Or 'http://localhost:3002/persons' if you changed the port
+      .then((response) => {
+        setPersons(response.data); // Update the state with the fetched data
+      })
+      .catch((error) => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
@@ -18,7 +30,8 @@ const App = () => {
     event.preventDefault();
     const newPerson = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1, // Generate a new ID (could be handled more elegantly)
     };
     setPersons([...persons, newPerson]);
     setNewName('');
